95f47d839727d316a43092dde4be18b908b2cb1f
Mirko Atanasov <mirko.atanasov@gmail.com>
2016-11-18 04:06:05 -0500

    my gTest commit through VS2015

---
+++ gTest/gTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gTest.Startup))]
+namespace gTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
